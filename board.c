ad361fafdedf9703aa4059e37ab4f637a61d2ea5
Nicholas Lantz <nlantz@mines.edu>
2016-03-10 20:50:13 -0700

    Got the board generation working, huge commit, I'm sorry

--- board.c
+++ board.c
@@ -1,33 +1,188 @@
 
 
 #include "board.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-struct minesweeper_board generate_board(unsigned short rows,
+const static unsigned short ASCII_OFFSET = 48;
+
+void generate_mines(struct minesweeper_board *self);
+short place_mine(struct minesweeper_board *self);
+void get_num_mine_neighbors(struct cell *self, struct minesweeper_board *board,
+        short row, short col);
+short in_bounds(struct minesweeper_board *board, short row, short col);
+void populate_cell_data(struct minesweeper_board *self);
+
+struct minesweeper_board *generate_board(unsigned short rows,
        unsigned short cols, unsigned short num_mines) {
 
-    struct minesweeper_board *board = (minesweeper_board *)
-                                malloc(sizeof(minesweeper_board));
-    board->rows = rows;
-    board->cols = cols;
+    struct minesweeper_board *board = (struct minesweeper_board *)
+            malloc(sizeof(struct minesweeper_board));
+    board->num_rows = rows;
+    board->num_cols = cols;
     board->num_mines = num_mines;
-    board->rows = (row *) malloc(sizeof(row) * num_rows);
+    board->rows = (struct row *) malloc(sizeof(struct row) * board->num_rows);
 
-    row *r;
-    for (r=board->rows; r < board->rows + rows; r++) {
-        cell *c;
+    struct row *r;
+    for (r=board->rows; r < board->rows + board->num_rows; r++) {
+        r->cells = (struct cell *) malloc(sizeof(struct cell) * board->num_cols);
+    }
 
-        for (c = r; c < r + cols; c++) {
+    generate_mines(board);
+    populate_cell_data(board);
 
+    return board;
+}
+
+void reveal_cell(struct minesweeper_board *self, unsigned short row,
+        unsigned short col) {
+
+}
+
+void flag_cell(struct minesweeper_board *self, unsigned short row,
+        unsigned short col) {
+
+
+}
+
+/* This function will not generate mines if the number of mines to place is
+ * greater than number of tiles on board, it'll print an error message if the
+ * caller tries to do that cause it's nice */
+
+void generate_mines(struct minesweeper_board *self) {
+    // naive implementation for now.
+    if (self->num_mines > self->num_rows * self->num_cols) {
+        fprintf(stderr, "Error: number of mines to place exceeds "
+                 "total number of tiles\n");
+        return;
+    }
+
+    unsigned short mines_to_place = self->num_mines;
+
+    while (mines_to_place > 0) {
+        if (place_mine(self)) {
+            // successfully place mine
+            mines_to_place--;
+        } else {
+            continue;
         }
     }
 }
 
-void reveal_cell(minesweeper_board *self, unsigned short row,
-        unsigned short col) {
 
+/* Attempts to place a mine, returns 1 on success, 0 on failure
+ */
+short place_mine(struct minesweeper_board *self) {
+    short row = rand() % self->num_rows;
+    short col = rand() % self->num_cols;
+
+    //if (self->rows[row]->cells[col].type == MINE) {
+    if (self->rows[row].cells[col].type == MINE) return 0;
+    else {
+        self->rows[row].cells[col].type = MINE;
+        return 1;
+    }
 }
 
-void flag_cell(minesweeper_board *self, unsigned short row,
-        unsigned short col) {
+void populate_cell_data(struct minesweeper_board *self) {
 
+    unsigned short i, j;
+    for(i = 0; i < self->num_rows; i++) {
+        for(j = 0; j < self->num_cols; j++) {
+            struct cell *c = ((self->rows + i)->cells + j);
+            c->status = HIDDEN;
+            get_num_mine_neighbors(c, self, i, j);
+        }
+    }
+}
+
+void get_num_mine_neighbors(struct cell *self, struct minesweeper_board *board,
+        short row, short col) {
+
+    self->num_mine_neighbors = 0;
+
+    static short i, j;
+    for(i = -1; i <= 1; i++) {
+        for(j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) continue;
+            if (!in_bounds(board, row+i, col+j)) continue;
+
+            if (board->rows[row+i].cells[col+j].type == MINE) {
+                self->num_mine_neighbors++;
+            }
+        }
+    }
+}
+
+short in_bounds(struct minesweeper_board *board, short row, short col) {
+    if (row < 0 || col < 0) {
+        return 0;
+    }
+    if (row > board->num_rows-1 || col > board->num_cols-1) {
+        return 0;
+    }
+
+    return 1;
+}
+
+void print_board_revealed(struct minesweeper_board *self) {
+    struct row *r;
+    unsigned int i;
+    unsigned int num_chars_to_print = (self->num_cols * 4) + 1;
+
+    for(i = 0; i < num_chars_to_print; i++) {
+        printf("%s", "─");
+    }
+    printf("\n");
+
+    for(r=self->rows; r < self->rows + self->num_rows; r++) {
+        
+        struct cell *c;
+        for(c = r->cells; c < r->cells + self->num_cols; c++) {
+            printf("%s %c ", "│", char_cell(c, 0));
+        }
+
+        printf("%s\n", "│");
+        for(i = 0; i < num_chars_to_print; i++) {
+            printf("%s", "─");
+        }
+
+        printf("\n");
+    }
+}
+
+char char_cell(struct cell *self, short should_hide) {
+    if (should_hide) {
+        if (self->status == HIDDEN) return ' ';
+    } else {
+        if (self->status == FLAGGED) return 'F';
+        if (self->type == MINE) return 'M';
+        if (self->num_mine_neighbors == 0) return ' ';
+    }
+
+    return (char) (self->num_mine_neighbors + ASCII_OFFSET);
+}
+
+char char_cell_type(enum cell_type self) {
+    switch (self) {
+        case CLEAR:
+            return ' ';
+        case MINE:
+            return 'X';
+        default:
+            return '!';
+    }
+}
+
+char char_cell_status(enum cell_status self) {
+    switch (self) {
+        case HIDDEN:
+            return '.';
+        case REVEALED:
+            return ' ';
+        case FLAGGED:
+            return 'F';
+        default:
+            return '!';
+    }
 }
